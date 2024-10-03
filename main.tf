108e84dbc70b83645a9a0035e35f48aefde25718
Kevin Cochran <40193276+kevincloud@users.noreply.github.com>
2024-10-03 16:51:20 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "project" {
+  for_each = var.student_emails
+  source   = "./modules/workshop-project"
+
+  project_name_prefix         = var.project_name_prefix
+  environment_key             = var.environment_key
+  environment_name            = var.environment_name
+  student_email               = each.key
+  expires_after_days          = var.expires_after_days
+  student_organization_key    = var.student_organization_key
+  sandbox_id                  = var.sandbox_id
+  sandbox_dynamodb_table_name = data.terraform_remote_state.infrastructure.outputs.sandbox_table_name
+  student_dynamodb_table_name = data.terraform_remote_state.infrastructure.outputs.users_table_name
+  login_base_url              = data.terraform_remote_state.infrastructure.outputs.users_login_url
+}
---
+++ modules/workshop-project/dynamodb.tf
@@ -0,0 +1,54 @@
+resource "aws_dynamodb_table_item" "this" {
+  count      = var.create_team_member ? "1" : 0
+  table_name = var.student_dynamodb_table_name
+  hash_key   = "ProjectId"
+
+  item = <<ITEM
+{
+    "ProjectId": { 
+        "S": "${local.project_id}"
+    },
+    "ProjectKey": { 
+        "S": "${local.project_key}" 
+    },
+    "ProjectName": { 
+        "S": "${local.project_name}"
+    },
+    "LoginEmail": { 
+        "S": "${local.login_email}" 
+    },
+    "LoginSecret": { 
+        "S": "${random_password.login_secret.result}" 
+    },
+    "LoginExpiresAt": {
+        "N": "${time_offset.login_expires_at.unix}"
+    },
+    "StudentEmail": {
+        "S": "${local.student_email}"
+    },
+    "StudentOrganizationKey": { 
+        "S": "${var.student_organization_key}" 
+    }
+}
+ITEM
+}
+
+resource "aws_dynamodb_table_item" "second" {
+  count      = var.create_team_member ? "1" : 0
+  table_name = var.sandbox_dynamodb_table_name
+  hash_key   = "SandboxId"
+
+  item = <<ITEM
+{
+    "SandboxId": {
+        "S": "${var.sandbox_id}"
+    },
+    "LoginUrl": {
+        "S": "${local.login_url}"
+    },
+    "ProjectId": { 
+        "S": "${local.project_id}"
+    }
+}
+ITEM
+}
